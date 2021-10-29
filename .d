7c9b33905b6419232e9644fe1fba19d397d8c4ce
cihonemm <79941004+cihonemm@users.noreply.github.com>
2021-10-29 04:00:16 +0300

    Create .d

---
+++ .d
@@ -0,0 +1,11 @@
+if game.PlaceId == 855499080 then
+loadstring(game:HttpGet('https://raw.githubusercontent.com/otuzbiy/hu/main/sky'))()
+else
+print("Skywars downloaded")
+end
+
+if game.PlaceId == 283721918 then
+loadstring(game:HttpGet('https://raw.githubusercontent.com/otuzbiy/hu/main/testsky'))()
+else
+print("Test downloaded")
+end
